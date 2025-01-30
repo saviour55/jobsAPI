eb995b5d28eaf584dbeed14b605110a194e37934
saviour55 <kuzzy1451@gmail.com>
2025-01-30 16:29:37 +0100

    Moddified Database config file for Production environement

--- database/configuration.js
+++ database/configuration.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize')
 require('dotenv').config()
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD, {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: false,
 });
