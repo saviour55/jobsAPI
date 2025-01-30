b31b4be3def8cf594f6cd218470b751a7d72ce5c
saviour55 <kuzzy1451@gmail.com>
2025-01-30 16:18:23 +0100

    Moddified Database config file for Production environement

--- database/configuration.js
+++ database/configuration.js
@@ -1,7 +1,8 @@
 const {Sequelize} = require('sequelize')
+require('dotenv').config()
 
 
-const sequelize = new Sequelize('jobsapi', 'root', '', {
+const sequelize = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
